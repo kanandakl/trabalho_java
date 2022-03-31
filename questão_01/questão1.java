79a2699c573d928a2cc70c8e3636f241a877094d
kanandakl <kanandalarry2018@gmail.com>
2022-03-31 19:57:32 -0300

    trabalho_java

---
+++ questão_01/questão1.java
@@ -0,0 +1,15 @@
+package trabalho_java;
+
+public class questao1 {
+
+	public static void main(String[]args) {
+		 int r = 6;
+	    double P = 3.1416; 
+		double a = P *(r*r);
+		System.out.println(a);
+		
+		
+		
+		
+	}
+}
---
+++ questão_02/questão2.java
@@ -0,0 +1,22 @@
+package trabalho_java;
+
+public class questao2 {
+
+	public static void main(String[] args) {
+		double a = 4.2;
+		double b = 5.6;
+		double c = 8.7;
+		double PI = 3.14159 ;
+		double at = (a * c) / 2;
+		double ac = PI * (c*c);
+		double atr = (a + b) * c / 2 ; 
+		double aq = b * b ;
+		double ar = a * b;
+		System.out.println("area: " + at);
+		System.out.println("area: " + ac);
+		System.out.println("area: " + atr);
+		System.out.println("area: " + aq);
+		System.out.println("area: " + ar);
+		
+	}
+}
